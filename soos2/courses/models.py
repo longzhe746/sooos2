02e06c8167d4689e101295fcf61f18f96390285d
lzhong <longzhe746@foxmail.com>
2018-01-09 14:45:30 +0800

    2018-01-09

--- soos2/courses/models.py
+++ soos2/courses/models.py
@@ -3,9 +3,11 @@ from django.db import models
 from django.utils import  timezone
 
 # Create your models here.
+
+
 class Course(models.Model):
-    name = models.CharField(max_length=50,verbose_name='课程名')
-    desc = models.CharField(max_length=300,verbose_name='课程描述')
+    name = models.CharField(max_length=50, verbose_name='课程名')
+    desc = models.CharField(max_length=300, verbose_name='课程描述')
     detail = models.TextField(verbose_name='课程详情')
     degree =  models.CharField(choices=(('cj','初级'),('zj','中级'),('gj','高级')),max_length=2)
     learn_times = models.IntegerField(default=0,verbose_name='学习时长（分钟）')
@@ -17,6 +19,9 @@ class Course(models.Model):
 
     def __str__(self):
         return self.name
+
     class Meta:
-        verbose_name = ''
+        verbose_name = '课程'
         verbose_name_plural = verbose_name
+
+
