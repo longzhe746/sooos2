e8a96893e4dcdddc688b00e031be2a959f02c225
lzhong <longzhe746@foxmail.com>
2018-01-29 15:37:12 +0800

    2018-01-29

--- soos2/apps/users/adminx.py
+++ soos2/apps/users/adminx.py
@@ -1,6 +1,20 @@
 # -*- coding:utf-8 -*-
 import xadmin
 from .models import EmailverifyRecord, Banner
+from xadmin.views import  BaseAdminView, CommAdminView
+
+'''
+setting for xadmin main functions
+'''
+class BaseSetting(object):
+    enable_themes = True
+    use_bootswatch =True
+
+
+class GlobalSettings(object):
+    site_title = 'sooos.net'
+    site_footer = '速速走遍世界'
+    menu_style = 'accordion'
 
 
 class EmailverifyRecordAdmin(object):
@@ -20,3 +34,5 @@ class BannerAdmin(object):
 
 xadmin.site.register(EmailverifyRecord,EmailverifyRecordAdmin)
 xadmin.site.register(Banner,BannerAdmin)
+xadmin.site.register(BaseAdminView,BaseSetting)
+xadmin.site.register(CommAdminView,GlobalSettings)
