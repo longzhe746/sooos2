8736939a3eb1542362a1eb3e6fc3ae5ccc69f614
lzhong <longzhe746@foxmail.com>
2018-02-02 16:59:47 +0800

    2018-02-02

--- soos2/apps/users/views.py
+++ soos2/apps/users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get['username']
+        password = request.POST.get['password']
+
+    elif request.method == 'GET':
+        return render(request, 'login.html', {})
